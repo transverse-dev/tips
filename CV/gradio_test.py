b327107df73939b8596383c0411e6f8f697b34dc
Twave1717 <wjdrldyd0213@gmail.com>
2024-01-08 15:20:21 +0900

    gradio->cv2로 툴 이전 확정

--- CV/gradio_test.py
+++ CV/gradio_test.py
@@ -1,3 +1,10 @@
+
+################
+# Gradio는 비디오 송출이 너무 느려서 Real-Time Streaming이 불가합니다.
+# CV2로 툴을 바꿔서 작업하겠습니다.
+################
+
+
 import cv2
 import gradio as gr
 
@@ -9,7 +16,7 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 
-def process_image(audio):
+def process_image(_):
     # 여기에서 웹캠 프레임을 사용하여 이미지 처리를 수행
     # 이 예제에서는 텍스트를 받아와서 OpenCV 창에 표시
 
@@ -22,7 +29,7 @@ def process_image(audio):
     # 이미지 크기를 반으로 줄임
     frame = cv2.resize(frame, (320, 240))
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    yield frame, "text_input"
+    return frame, "text_input"
 
 # Gradio 웹 인터페이스 정의
 with gr.Blocks() as demo:
@@ -30,11 +37,11 @@ with gr.Blocks() as demo:
         input_audio = gr.Audio()
         image = gr.Image(streaming=True)
         output = gr.Text()
-        input_audio.stream(process_image, [input_audio], [image, output])
+        input_audio.stream(process_image, [input_audio], [image, output]) # , every=0.1 -> frame 높아지지만 서버 강제종료
 
 
 # Gradio 웹 인터페이스 실행
-demo.launch()
+demo.queue().launch()
 
 # 프로그램 종료 시 웹캠 객체 해제
 cap.release()
