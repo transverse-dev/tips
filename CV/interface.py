864238243430731bae97368859bd55aa21aa3486
Twave1717 <wjdrldyd0213@gmail.com>
2024-01-21 19:58:00 +0900

    face_landmark

--- CV/interface.py
+++ CV/interface.py
@@ -1,32 +1,60 @@
 import cv2
+import numpy as np
+import dlib
 
-# 웹캠 캡처 객체 생성
-cap = cv2.VideoCapture(0)
+def print_face(facial_landmarks, _gray, _frame):
+    face_region = np.array([(facial_landmarks.part(0).x, facial_landmarks.part(0).y),
+                            (facial_landmarks.part(1).x, facial_landmarks.part(1).y),
+                            (facial_landmarks.part(2).x, facial_landmarks.part(2).y),
+                            (facial_landmarks.part(3).x, facial_landmarks.part(3).y),
+                            (facial_landmarks.part(4).x, facial_landmarks.part(4).y),
+                            (facial_landmarks.part(5).x, facial_landmarks.part(5).y),
+                            (facial_landmarks.part(6).x, facial_landmarks.part(6).y),
+                            (facial_landmarks.part(7).x, facial_landmarks.part(7).y),
+                            (facial_landmarks.part(8).x, facial_landmarks.part(8).y),
+                            (facial_landmarks.part(9).x, facial_landmarks.part(9).y),
+                            (facial_landmarks.part(10).x, facial_landmarks.part(10).y),
+                            (facial_landmarks.part(11).x, facial_landmarks.part(11).y),
+                            (facial_landmarks.part(12).x, facial_landmarks.part(12).y),
+                            (facial_landmarks.part(13).x, facial_landmarks.part(13).y),
+                            (facial_landmarks.part(14).x, facial_landmarks.part(14).y),
+                            (facial_landmarks.part(15).x, facial_landmarks.part(15).y),
+                            (facial_landmarks.part(16).x, facial_landmarks.part(16).y),
+                            (facial_landmarks.part(18).x, facial_landmarks.part(18).y),
+                            (facial_landmarks.part(23).x, facial_landmarks.part(23).y)], np.int32)
 
-# 웹캠 해상도 설정 (원하는 해상도로 조절)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cv2.polylines(_frame, [face_region], True, (0, 255, 255), 1)
 
-while True:
-    # 웹캠에서 프레임 읽기
-    ret, frame = cap.read()
+def main():
 
-    if not ret:
-        print("Failed to capture frame")
-        break
+    cap = cv2.VideoCapture(0)
+    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-    # 여기에서 추가적인 프레임 처리를 할 수 있음
-    # ...
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to capture frame")
+            break
 
-    # 프레임을 화면에 표시
-    cv2.imshow("Webcam", frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = detector(gray)
+        face = faces[0]
 
-    # 'q' 키를 누르면 종료
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        landmarks = predictor(gray, face)
+        print_face(landmarks, gray, frame)
 
-# 프로그램 종료 시 웹캠 객체 해제
-cap.release()
+        cv2.imshow("Webcam", frame)
 
-# OpenCV 창 닫기
-cv2.destroyAllWindows()
+        # 'q' 키를 누르면 종료
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+   main()
